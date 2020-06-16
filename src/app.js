a358614304eb0b271eea98bac185e0617648390b
mauaa <mauaa@koder.com>
2020-06-16 12:12:07 +0530

    Set private routes

--- src/app.js
+++ src/app.js
@@ -2,16 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // import 'react-dates/initialize';
 import { Provider } from 'react-redux';
-import AppRouter from './routers/AppRouter';
+import AppRouter, { history } from './routers/AppRouter';
 import configureStore from './store/configureStore';
 import { addExpense } from './actions/expenses'
 import { startSetExpenses } from './actions/expenses';
-import { setTextFilter } from './actions/filters'
+import { login, logout } from './actions/auth';
 import getVisibleExpenses from './selectors/expenses'
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import LoadingPage from './components/LoadingPage';
-import './firebase/firebase';
+import { firebase } from './firebase/firebase';
 // const Layout = (props) => {
 //     return (
 //         <div>
@@ -50,10 +50,23 @@ const renderApp = () => {
 };
 
 ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
-store.dispatch(startSetExpenses()).then(() => {
-    ReactDOM.render(jsx, document.getElementById('app'));
-});
 
+
+firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+        store.dispatch(login(user.uid));
+        store.dispatch(startSetExpenses()).then(() => {
+            renderApp();
+            if (history.location.pathname === '/') {
+                history.push('/dashboard');
+            }
+        });
+    } else {
+        store.dispatch(logout());
+        renderApp();
+        history.push('/');
+    }
+});
 
 
 
